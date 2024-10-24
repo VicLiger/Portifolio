9bc160b3c9b8bd28a6dd5975d35005091aa22f4d
Victor <victor.liger0800@gmail.com>
2024-10-23 22:19:00 -0300

    ♻️ refactor: refatorando o header

--- src/app/components/header-bar/header-bar.component.ts
+++ src/app/components/header-bar/header-bar.component.ts
@@ -6,25 +6,28 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './header-bar.component.html',
-  styleUrls: ['./header-bar.component.css']
+  styleUrls: ['./header-bar.component.css'] // Corrigido para styleUrls
 })
 export class HeaderBarComponent implements OnInit {
-
-  options: string[] = ["Home", "About", "Skills", "Projects", "Contact"];
-  activeIndex: number | null = null; // Indica o índice do item ativo
+  
+  optionsList: string[] = ["Home", "About", "Technologies", "Projects", "Contact"];
+  activeItem: number | null = null; // Armazena o item ativo
+  isMenuVisible: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void { }
 
-  isClicked(index: number): boolean {
-    return this.activeIndex === index;
+  setActive(item: number): void {
+    this.activeItem = item; // Define o item clicado como ativo
   }
 
-  setActive(index: number): void {
-    if (this.activeIndex === index) {
-      return; 
-    }
-    this.activeIndex = index; 
+  isClicked(item: number): boolean {
+    return this.activeItem === item; // Verifica se o item é o ativo
   }
+
+  toggleMenu() {
+    this.isMenuVisible = !this.isMenuVisible; 
+    console.log('Menu visibility:', this.isMenuVisible); // Log para verificar o estado
+}
 }

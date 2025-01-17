5d273c865288cbf4df1a784042f73ac3c78e74ed
Victor <victor.liger0800@gmail.com>
2025-01-17 19:00:18 -0300

    🚧 adicionando 2 components de apresentação central

---
+++ src/app/Components/basic-presentation/basic-presentation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-presentation',
+  imports: [],
+  templateUrl: './basic-presentation.component.html',
+  styleUrl: './basic-presentation.component.css'
+})
+export class BasicPresentationComponent {
+
+}
---
+++ src/app/Components/container-image/container-image.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-container-image',
+  imports: [],
+  templateUrl: './container-image.component.html',
+  styleUrl: './container-image.component.css'
+})
+export class ContainerImageComponent {
+
+}
